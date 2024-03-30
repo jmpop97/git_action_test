22c3fb5d4ba54c07c310e597a0451775c5549ea8
jmpop <pjm970128@gmail.com>
2024-03-30 15:55:58 +0900

    .

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-print("파일wnd")
+print("파일wjwkd")
 with open("저장.txt",mode="w",encoding='utf-8') as f:
     f.write("저장완료")
+    print("저장완료")
 with open('저장.txt',mode="r",encoding='utf-8') as f:
     print(f)
